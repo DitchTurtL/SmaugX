39f46dd53b1a68a86dbc550eae30c0e590a2a6ed
DitchTurtL <ridium@gmail.com>
2024-01-23 05:27:35 +0000

    Moving logging initializer

--- SmaugX.Host/Program.cs
+++ SmaugX.Host/Program.cs
@@ -6,7 +6,22 @@
 ///   Main appliation entry point.
 ///
 
+using Serilog;
+using Serilog.Sinks.File;
 using SmaugX.Core.Hosting;
+using SmaugX.Core.Constants;
+
+// Verify log directory exists
+var logPath = Path.Combine(Environment.CurrentDirectory, FileConstants.LOG_DIRECTORY);
+if (!Directory.Exists(logPath))
+    Directory.CreateDirectory(FileConstants.LOG_DIRECTORY);
+
+// Start serilogger
+var logFile = Path.Combine(logPath, FileConstants.LOG_FILE);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(logFile, rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 // Start Core services
 await SystemInitializer.StartUp();
@@ -16,3 +31,6 @@ await Server.Start();
 
 // Shutdown Core services
 await SystemInitializer.ShutDown();
+
+
+await Log.CloseAndFlushAsync();
