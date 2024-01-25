99f2c63532b6d3f781af32022727857a73206025
DitchTurtL <ridium@gmail.com>
2024-01-24 21:46:15 -0500

    Build command handler

---
+++ SmaugX.Core/Commands/CommandHandlers/BuildHandler.cs
@@ -0,0 +1,26 @@
+﻿using SmaugX.Core.Services;
+
+namespace SmaugX.Core.Commands.CommandHandlers;
+
+internal class BuildHandler : ICommandHandler
+{
+    private readonly IRoomService roomService;
+
+    public BuildHandler(IRoomService roomService)
+    {
+        this.roomService = roomService;
+    }
+
+    public void HandleCommand(ICommand command)
+    {
+        // Don't process this if it's not a build command.
+        if (command.Name.ToUpper() != "BUILD")
+            return;
+
+
+
+
+
+
+    }
+}
